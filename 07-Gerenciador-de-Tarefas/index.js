943ef864ec0c4ef67a86e5f8fbc319a9c7a4f67d
Levi Santos <marcosmcod@gmail.com>
2026-08-07 18:47:13 -0300

    Adicionei a função de limpar os campos

--- 07-Gerenciador-de-Tarefas/index.js
+++ 07-Gerenciador-de-Tarefas/index.js
@@ -6,6 +6,11 @@ const botao = document.querySelector("#BotaoLançarTarefas");
 const resultado = document.querySelector("#listaTarefas");
 const ul = document.querySelector("#Pailista");
 
+function LimparCampos() {
+        nomeDaTarefa.value = "";
+        CategoriaDaTarefa.value = "";
+        Quantidade.value = "";
+    }
 
 botao.addEventListener('click', () => {
     const id = Date.now();
@@ -16,7 +21,8 @@ botao.addEventListener('click', () => {
     listadeTarefas.push(NovaTarefa);
     resultado.textContent = "As suas tarefas adicionadas foram: " + NovaTarefa.nomeDaTarefa;
     resultado.insertAdjacentHTML('afterend', `<p>${NovaTarefa.nomeDaTarefa}</p>`);
-
+    
+    return LimparCampos();
 });
 
 class item {
